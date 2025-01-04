a2c83416a607579441b63f67711b0363339d9ae5
Wojciech <wojciechp99@outlook.com>
2025-01-04 13:33:23 +0100

    set 700 for max width

--- result/task3.py
+++ result/task3.py
@@ -2,15 +2,14 @@ import csv
 import pandas as pd
 
 if __name__ == '__main__':
-    file_path = "dev-0/in.tsv"
+    file_path = "../dev-0/in.tsv"
     data = []
     with open(file_path, mode='r', encoding='utf-8') as file:
-        reader = csv.reader(file, delimiter='\t')  # Użyj tabulacji jako separatora
+        reader = csv.reader(file, delimiter='\t')
         for row in reader:
             data.append({'doc_id': row[0], 'page_num': row[1], 'year': row[2], 'ocr_text': row[3]})
 
-        # Convert to pandas DataFrame for easier processing
         df = pd.DataFrame(data)
+        pd.set_option('display.max_colwidth', 700)
 
-        # Example: Display the first row
         print(df.head())
